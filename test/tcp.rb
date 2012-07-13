6c7051ee6e5e40b47b9dd8fcea8acf940bcd47c1
Jacob Evans <jacob@dekz.net>
2012-07-13 14:34:09 +1000

    move to test folder.

---
+++ test/tcp.rb
@@ -0,0 +1,23 @@
+require 'socket'
+require 'middleware'
+require File.join(File.dirname(__FILE__), 'adapters/commands')
+require File.join(File.dirname(__FILE__), 'adapters/executors')
+require File.join(File.dirname(__FILE__), 'adapters/repositories')
+require File.join(File.dirname(__FILE__), 'adapters/reporters')
+
+server = TCPServer.new 3333
+loop do
+
+  client = server.accept
+  cmd =  client.readline
+  cmd = cmd.strip
+
+  stack = Middleware::Builder.new do
+    use Commands
+    use Repositories
+    use Executors
+    use Reporters
+  end
+
+  stack.call({ :cmd => cmd.to_sym })
+end
