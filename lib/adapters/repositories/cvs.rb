3b548485beaaea8eb2ff5867ae38382b453c9cca
Jacob Evans <jacob@dekz.net>
2012-07-19 14:32:52 +1000

    Add -P to cvs co

--- lib/adapters/repositories/cvs.rb
+++ lib/adapters/repositories/cvs.rb
@@ -11,6 +11,7 @@ module Repository
     def call(env)
       log :cvs_before, :type => :debug
       repos = env[:env]
+      p repos
       perform(repos) if repos[:type] == 'cvs'
       @app.call(env)
       log :cvs_end, :type => :debug
@@ -19,13 +20,14 @@ module Repository
     def perform args
       repo = args
       op = repo[:operation] || 'co'
-      cvs_args = repo[:options] if repo.member? :options
+      cvs_args = repo.member?(:options) ? repo[:options] : ''
       path = repo[:path]
 
       cmd = CommandBuilder.new(:cvs)
+      cmd << op
+      cmd << path
       cmd << cvs_args unless cvs_args.empty?
-      cmd << op unless op.empty?
-      cmd << path unless path.empty?
+      cmd << :P if cvs_args.empty?
 
       fexec cmd.to_s
     end
