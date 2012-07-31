287c83a30ab8c1b75196ad35ee94bd8922caa122
Jacob Evans <jacob@dekz.net>
2012-07-31 16:18:59 +1000

    update default executor

--- lib/adapters/executors/exec.rb
+++ lib/adapters/executors/exec.rb
@@ -1,16 +1,20 @@
 module Executor
   class DefaultExecutor
+    include Logger
+    include Executor
+
     def initialize(app)
       @app   = app
     end
 
     def call(env)
-      puts "I shall exec #{env[:exec]}" if env[:exec] == 'exec'
+      log "I shall exec #{env[:exec]}" if env[:exec] == 'exec'
+      perform(env[:exec])
       @app.call(env)
     end
 
     def perform args
-      exec args
+      resp = fexec args
     end
   end
 end
